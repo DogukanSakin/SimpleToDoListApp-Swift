5068973a40f36426452677bddbe8ea5924ed3024
DogukanSakin <dogukansakin1@hotmail.com>
2023-05-19 18:22:15 +0300

    Functional improvements.

--- ToDoListApp/Controllers/ViewController.swift
+++ ToDoListApp/Controllers/ViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+import CoreData
+class ViewController: UIViewController  {
     
     @IBOutlet weak var tableView : UITableView!
     
-    var todos = [String]()
+    var todos = [NSManagedObject]()
     
     var alertController = UIAlertController()
     
@@ -19,31 +19,34 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        fetchTodoData()
         
     }
     
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return todos.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
-        
-        cell.textLabel?.text = todos[indexPath.row]
-        return cell
-    }
-    
+
     @IBAction func didTapAddButton(_ sender: UIBarButtonItem){
         
         presentAlert(title: "Add a new item",
                      message: "Please write your to do item content.",
-                     defaultButtonTitle:"Add",defaultButtonAction: { _ in
+                     defaultButtonTitle:"Add",
+                     defaultButtonAction: { _ in
             let text = self.alertController.textFields?.first?.text
             
             if(text != ""){
-                self.todos.append(text!)
-                self.tableView.reloadData()
+               
+                
+                let appDelegate = UIApplication.shared.delegate as? AppDelegate
+                
+                let managedObjectContext = appDelegate?.persistentContainer.viewContext
+                
+                let entity = NSEntityDescription.entity(forEntityName: "ListItem", in: managedObjectContext!)
+                
+                let content = NSManagedObject(entity: entity!, insertInto: managedObjectContext!)
+                
+                content.setValue(text, forKey: "content")
+                
+                self.fetchTodoData()
+              
             }
             else{
                 self.presentAlert(title: "Error",
@@ -56,6 +59,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         
         
+    }
+    
+    @IBAction func didTapDeleteAllButton(_ sender: UIBarButtonItem){
+        presentAlert(title: "Are you sure?",
+                     message: "Your all items will delete. Are you sure?",
+                     defaultButtonTitle: "Delete All") { _ in
+            let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            let managedObjectContext = appDelegate?.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ListItem")
+            let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+            do{
+                try managedObjectContext!.execute(deleteRequest)
+                try managedObjectContext!.save()
+                self.todos.removeAll()
+                self.tableView.reloadData()
+            }
+            catch{
+                print(error)
+            }
+            
+        }
     }
     
     func presentAlert(title: String?, message: String?, defaultButtonTitle: String? = nil, defaultButtonAction: ((UIAlertAction) -> Void)? = nil, isEnabledInputArea: Bool? = false){
@@ -80,6 +104,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         if isEnabledInputArea! {
             alertController.addTextField()
+            
         }
         
         
@@ -89,9 +114,75 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     
+    func fetchTodoData(){
+        print("fetch data...")
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let managedObjectContext = appDelegate?.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "ListItem")
+        do{
+            todos = try managedObjectContext!.fetch(fetchRequest)
+            self.tableView.reloadData()
+        }
+        catch{
+            print(error)
+        }
+    }
     
     
     
+}
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
+        let listItem = todos[indexPath.row]
+        cell.textLabel?.text = listItem.value(forKey: "content") as? String
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let managedObjectContext = appDelegate?.persistentContainer.viewContext
+        
+        let deleteAction = UIContextualAction(style: UIContextualAction.Style.normal, title: "Delete") { _, _, _ in
+            managedObjectContext?.delete(self.todos[indexPath.row])
+            try? managedObjectContext?.save()
+            self.fetchTodoData()
+        }
+        let editAction = UIContextualAction(style: UIContextualAction.Style.normal, title: "Edit") { _, _, _ in
+            self.presentAlert(title: "Edit the item",
+                         message: nil,
+                         defaultButtonTitle:"Save",
+                         defaultButtonAction: { _ in
+          
+                let text = self.alertController.textFields?.first?.text
+                
+                if(text != ""){
+                    self.todos[indexPath.row].setValue(text, forKey: "content")
+                    if managedObjectContext!.hasChanges {
+                        try? managedObjectContext?.save()
+                    }
+                    self.tableView.reloadData()
+                }
+                else{
+                    self.presentAlert(title: "Error",
+                                      message: "Please enter a valid to do.")
+                }
+                
+                
+            }, isEnabledInputArea: true
+            )
+            
+        }
+        deleteAction.backgroundColor = .systemRed
+        editAction.backgroundColor = .systemBlue
+        let config = UISwipeActionsConfiguration(actions: [deleteAction,editAction])
+        return config
+    }
 }
 
